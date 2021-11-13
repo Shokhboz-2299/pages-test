2594e23d3d705837aefdd22677ab5d920a6413f4
Shokhboz-2299 <shokhbozkaumutov@gmail.com>
2021-11-13 08:59:05 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis fugiat iure sunt. Pariatur fugiat, perspiciatis eveniet quia rem dicta expedita dolorum odio praesentium debitis non assumenda omnis similique blanditiis? Maiores.</p>
+    </>
+  );
+}
+
+export default App;
